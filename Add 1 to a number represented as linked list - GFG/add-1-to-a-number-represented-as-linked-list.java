b35b4d8dee929c52383b32f5ab8492b9c36fc372
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-04-19 00:38:35 +0530

    Added solution - LeetHub

---
+++ Add 1 to a number represented as linked list - GFG/add-1-to-a-number-represented-as-linked-list.java
@@ -0,0 +1,98 @@
+// { Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+class GfG
+{
+    public static void printList(Node node) 
+    { 
+        while (node != null)
+        { 
+            System.out.print(node.data);
+            node = node.next; 
+        }  
+        System.out.println();
+    } 
+    public static void main(String args[])throws IOException
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Node head = new Node( s.charAt(0) - '0' );
+                    Node tail = head;
+                    for(int i=1; i<s.length(); i++)
+                    {
+                        tail.next = new Node( s.charAt(i) - '0' );
+                        tail = tail.next;
+                    }
+                    Solution obj = new Solution();
+                    head = obj.addOne(head);
+                    printList(head); 
+                }
+        }
+}// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+} 
+*/
+
+class Solution
+{
+    public static Node addOne(Node head) 
+    { 
+      head =  reverse(head);
+       Node cur = head;
+      
+       while(cur != null ){
+              if(cur.next == null && cur.data == 9){
+                  cur.data = 0;
+                  Node temp = new Node(0);
+                  cur.next = temp;
+                  temp = head;
+                  cur = cur.next;
+              }else if(cur.data == 9){
+                  cur.data = 0;
+                  cur = cur.next;
+              }else{
+                   cur.data = cur.data + 1;
+                   cur = cur.next;
+                   break;
+               }
+        }
+          head = reverse(head);
+               return head;
+    }
+    public static Node reverse(Node head){
+        Node curr = head;
+        Node prev = null;
+        while(curr != null){
+            Node temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+        
+    }
+}
