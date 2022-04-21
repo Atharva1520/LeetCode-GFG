2347045c6371780dcd45c9fe74359596ccb70f6c
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-04-21 17:22:50 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (26.86%) - LeetHub

--- 27-remove-element/27-remove-element.java
+++ 27-remove-element/27-remove-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-         int index = 0;
+         /*int index = 0;
         int result[] = Arrays.copyOf(nums, nums.length);
         for (int i = 0; i < result.length; i++) {
             if (result[i] != val) {
@@ -10,6 +10,21 @@ class Solution {
         }
 
 
-        return index;  
+        return index;  */
+        int length = nums.length;
+		int count = 0;
+		for (int i = 0; i < length; i++) {
+			while (nums[i] == val &&  i != length) {
+                if(length == 0) {
+					break;
+				}
+				int temp = nums[nums.length - 1 - count];
+				nums[nums.length - 1 - count] = nums[i];
+				nums[i] = temp;
+				count++;
+				length = length - 1;
+			}
+		}
+        return length;
     }
 }
