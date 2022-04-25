ede16ab2f40a0c516397ea5a3b0f871ac3d960a2
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-04-25 18:22:29 +0530

    Time: 1 ms (90.46%), Space: 42.3 MB (33.12%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+         if(sentence.length() < 26){
+            return false;
+        }
+        for(char i = 'a' ; i <= 'z' ; i++){
+            if(!sentence.contains(String.valueOf(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
