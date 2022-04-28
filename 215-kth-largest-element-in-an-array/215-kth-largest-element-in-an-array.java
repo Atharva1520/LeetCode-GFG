e0369cbc4c727ffee73b63e0990ca68159450c4b
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-04-29 00:54:16 +0530

    Time: 1 ms (99.49%), Space: 42.1 MB (98.08%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,13 +1,57 @@
-class Solution {
+public class Solution {
+    
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> ans = new PriorityQueue<Integer>(Collections.reverseOrder());
-        for(int i = 0 ; i < nums.length ; i++){
-            ans.add(nums[i]);
-        }
-        while(k > 1){
-            ans.remove(ans.peek());
-            k--;
-        }
-        return ans.peek();
+
+        return select(nums, k-1);
     }
+    
+    // Quick select
+    private int select(int[] nums, int k) {
+        int left = 0, right = nums.length-1;
+        while(true) {
+            if(left == right)
+                return nums[left];
+            int pivotIndex = medianOf3(nums, left, right);
+            pivotIndex = partition(nums, left, right, pivotIndex);
+            if(pivotIndex == k)
+                return nums[k];
+            else if(pivotIndex > k)
+                right = pivotIndex-1;
+            else
+                left = pivotIndex+1;
+        }
+    }
+    
+    //Use median-of-three strategy to choose pivot
+    private int medianOf3(int[] nums, int left, int right) {
+        int mid = left + (right - left) / 2;
+        if(nums[right] > nums[left])
+            swap(nums, left, right);
+        if(nums[right] > nums[mid])
+            swap(nums, right, mid);
+        if(nums[mid] > nums[left])
+            swap(nums,left, mid);
+        return mid;
+    }
+    
+    private int partition(int[] nums, int left, int right, int pivotIndex) {
+        int pivotValue = nums[pivotIndex];
+        swap(nums, pivotIndex, right);
+        int index = left;
+        for(int i = left; i < right; ++i) {
+            if(nums[i] > pivotValue) {
+                swap(nums, index, i);
+                ++index;
+            }
+        }
+        swap(nums, right, index);
+        return index;
+    }
+    
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
 }
