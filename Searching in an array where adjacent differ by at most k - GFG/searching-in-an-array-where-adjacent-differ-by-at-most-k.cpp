3735df826236ad716f7c61f5aa84d9e7cbb3e7dd
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-06-16 23:59:34 +0530

    Added solution - LeetHub

---
+++ Searching in an array where adjacent differ by at most k - GFG/searching-in-an-array-where-adjacent-differ-by-at-most-k.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+// C++ program to search an element in an array
+// where difference between all elements is 1
+#include<bits/stdc++.h>
+using namespace std;
+
+// x is the element to be searched in arr[0..n-1]
+// such that all elements differ by at-most k.
+int search(int arr[], int n, int x, int k);
+
+
+// Driver program to test above function
+int main()
+{
+	int t,n,k,x;
+	cin>>t;
+	while(t--)
+    {
+
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        cin>>x;
+        cout<<search(arr, n, x, k)<<endl;
+
+    }
+return 0;
+}
+
+// } Driver Code Ends
+
+
+// x is the element to be searched in arr[0..n-1]
+// such that all elements differ by at-most k.
+int search(int arr[], int n, int x, int k)
+{
+   	int i = 0;
+    int jmp = 0;
+    while(i+jmp < n){
+        if(arr[i] == x)
+        return i;
+        
+        else{
+            int diff = abs(arr[i] - x);
+            jmp = diff/k;
+        }
+        
+        i += jmp==0 ? 1 : jmp;
+    }
+    return -1;
+}   
