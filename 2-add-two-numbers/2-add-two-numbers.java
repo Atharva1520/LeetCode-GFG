270f13e0fe20553a6102018334972cdf028af2fe
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-06-22 21:43:58 +0530

    Update 2-add-two-numbers.java

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -12,8 +12,8 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy= new ListNode();
         ListNode temp =dummy;
-        int carry = 0;
-        while(l1 != null || l2 != null || carry ==1){
+        int car = 0;
+        while(l1 != null || l2 != null || car ==1){
             int sum = 0 ;
             if(l1 != null){
                 sum += l1.val;
@@ -23,8 +23,8 @@ class Solution {
                 sum += l2.val;
                 l2 = l2.next;
             }
-            sum = sum + carry;
-            carry = sum / 10;
+            sum = sum + car;
+            car = sum / 10;
             
             ListNode node = new ListNode(sum % 10);
         temp.next = node;
