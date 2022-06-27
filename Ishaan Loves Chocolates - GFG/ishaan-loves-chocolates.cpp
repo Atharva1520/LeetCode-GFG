33eede47d8d01f374a019d0d005458eb577276d1
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-06-27 16:49:58 +0530

    Added solution - LeetHub

---
+++ Ishaan Loves Chocolates - GFG/ishaan-loves-chocolates.cpp
@@ -0,0 +1,40 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int chocolates(int arr[], int n);
+
+
+int main()
+{
+    
+    int t;cin>> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        
+        cout << chocolates(arr, n);
+        cout << endl;
+        
+    }
+
+}
+// } Driver Code Ends
+
+
+int chocolates(int arr[], int n)
+{
+    int mini = arr[0];
+   for(int i=1; i<n; i++) {
+       mini = min(mini, arr[i]);
+   }
+   return mini;
+    
+}
