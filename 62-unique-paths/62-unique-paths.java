d39bebc10e47d99bb8a1e3ddad4195bd4a7f0115
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-07-15 16:22:51 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (33.02%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,10 +1,10 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-       int N = n+m-2; //nCr  n = N
-       int r = m-1 ;// r = R
-           double res = 1;
-        for(int i = 1; i<=r;i++){
-            res = res * (N- r + i)/i;
+        int N = m+n-2;
+        int r = m - 1;
+        double res = 1 ;
+        for(int i =1; i <= r ; i++){
+            res = res * ( N - r + i)/i;
         }
         return (int) res;
     }
