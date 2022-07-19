ce452285b9763107e8cf13d7099b914b6af9363d
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-07-19 23:54:22 +0530

    Time: 19 ms (28.98%), Space: 46.9 MB (10.72%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,13 +1,32 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-      if(s.length()!=t.length()) return false;
+          if(s.length() != t.length()) return false;
         
-        for (int i=0; i<s.length(); i++) {
-            char curr = s.charAt(i);
-            t = t.replaceFirst(""+curr,"");
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch)){
+            int cf = map.get(ch);
+            int nf = cf + 1;
+            map.put(ch, nf);
+            }
+            else {
+                map.put(ch, 1);
+            }
         }
         
-        return t.length() == 0;
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            if(map.containsKey(ch) && map.get(ch) != 0){
+                int cf = map.get(ch);
+                int nf = cf - 1;
+                map.put(ch, nf);
+            } else{
+                return false;
+            }
+        }
         
+        return true;
     }
 }
