19f3972b8364425644b5cb208c663d5aa9fb5c42
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-08-22 22:24:38 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (87.93%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,35 +1,20 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-      //merge sort
-        int i=m-1;
-        int j = n-1;
-        int k=m+n-1;
-        while(i > -1 && j > -1){
-           if(nums1[i]>nums2[j])
-            {
-                nums1[k]=nums1[i];
-                i--;
+        int i=m;
+        int j=0;
+        while(i < nums1.length) nums1[i++] = nums2[j++];
+          int gap=((n+m)%2==1 && (n+m)!=1)?((n+m)/2+1):((n+m)/2);
+    while (gap!=0){
+        for( i =0 ; i < m+n-gap;i++){
+            if(nums1[i]>nums1[i+gap]){
+                int temp = nums1[i];
+                nums1[i]=nums1[i+gap];
+                nums1[i+gap]=temp;
             }
-            else
-            {
-                nums1[k]=nums2[j];
-                j--;
-            }
-            k--;
-            
+         
         }
-        while (i>-1)
-        {
-            nums1[k]=nums1[i];
-            i--;
-            k--;    
-        }
-	
-        while(j>-1)
-        {
-            nums1[k]=nums2[j];
-            j--;
-            k--;
-        }
-    }
+        gap=(gap%2==1 && gap!=1)?(gap/2+1):(gap/2);
+    }    
+        
+    }  
 }
