e30475189ad5068261b677ab1008be4f67f4393b
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-09-10 14:18:33 +0530

    Time: 2 ms (75.59%), Space: 48 MB (76.83%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.java
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -1,26 +1,19 @@
 class Solution {
-public:
-    vector<string> fizzBuzz(int n) {
-       vector<string>v;
-        for(int i=1;i<=n;i++)
-        {
-            if((i%3==0)&&(i%5!=0))
-            {
-                v.push_back("Fizz");
-                continue;
+    public List<String> fizzBuzz(int n) {
+         List<String> l = new ArrayList<String>();    //Make the Arraylist 
+		
+        for(int i=1;i<=n;i++){        // Iterate Loop n times to store the value
+            if(i%3==0 && i%5==0){     // Check for divisibility for 3 and 5
+                l.add("FizzBuzz");    //  add the word "FizzBuzz" at index i
+            }else if(i%3==0){       // Check for divisibility for 3
+                l.add("Fizz");         //  add the word "Fizz" at index i
+            }else if(i%5==0){    // Check for divisibility for 5
+                l.add("Buzz");      //  add the word "Buzz" at index i
+            }else{
+                String s = Integer.toString(i);  // convert Integer to String to store Integer in List
+                l.add(s);  // add Integer i at index i
             }
-            if((i%3!=0)&&(i%5==0))
-            {
-                v.push_back("Buzz");
-                continue;
-            }
-            if((i%3==0)&&(i%5==0))
-            {
-                v.push_back("FizzBuzz");
-                continue;
-            }
-            v.push_back(to_string(i));
         }
-        return v;
+        return l;
     }
-};
+}
