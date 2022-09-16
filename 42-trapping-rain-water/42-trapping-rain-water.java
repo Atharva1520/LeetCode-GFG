9051195b2160cbdf379d6539f35de4a1a2cbed8c
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-09-16 21:30:58 +0530

    Time: 3 ms (28.18%), Space: 48.7 MB (57.38%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] arr) {
+        int n = arr.length;
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+        prefix[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = Math.max(prefix[i - 1], arr[i]);
+        }
+        suffix[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = Math.max(suffix[i + 1], arr[i]);
+        }
+        int waterTrapped = 0;
+        for (int i = 0; i < n; i++) {
+            waterTrapped += Math.min(prefix[i], suffix[i]) - arr[i];
+        }
+        return waterTrapped;
+    }
+}
