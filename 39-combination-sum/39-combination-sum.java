0c411eb8e464b680999f2894de946339d04c1bff
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-09-19 14:56:48 +0530

    Time: 4 ms (86.90%), Space: 44.7 MB (81.12%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,24 +1,24 @@
 class Solution {
-    ArrayList<List<Integer>>  ans ;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       ans = new ArrayList<>();
-        find(0,target,candidates,new ArrayList<>());
+        List<List<Integer>> ans = new ArrayList<>();
+        f(0,candidates,new ArrayList<>(),ans,target);
         return ans;
+        
     }
-    public void find(int index,int target,int arr[],List<Integer> ds ){
-         
-        if(target == 0){
-           ans.add(new ArrayList<> (ds));
-            return ;
-        }else if(index == arr.length){
+    public void f(int ind,int[] nums,List<Integer> ds,List<List<Integer>> ans,int target){
+        if(ind == nums.length){
+            if(target ==0){
+                ans.add(new ArrayList<>(ds));
+            }
             return;
         }
-       
-        if(arr[index] <= target){
-            ds.add(arr[index]);
-            find(index,target - arr[index],arr,ds);
-            ds.remove(ds.size() - 1);
+        if(target >= nums[ind]){
+            //pick
+            ds.add(nums[ind]);
+            f(ind,nums,ds,ans,target-nums[ind]);
+            ds.remove(ds.size()-1);
         }
-        find(index + 1,target,arr,ds);
+        //notpick
+        f(ind+1,nums,ds,ans,target);
     }
 }
