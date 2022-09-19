4222144439f2bb140fb1d3c20ebfd83c6c93b28a
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-09-19 23:27:05 +0530

    Time: 30 ms (5.04%), Space: 58.5 MB (6.60%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,35 +1,37 @@
 class Solution {
-public int numIslands(char[][] grid) {
-int m = grid.length;
-int n = grid[0].length;
-
- boolean[][] visited = new boolean[m][n];
- 
- int count = 0;
+    public int numIslands(char[][] grid) {
+        Queue<int[]> q = new LinkedList<>();
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    q.add(new int[]{i, j});
+                    count += sinkIsland(grid, q);
+                }
+            }
+        }
+        return count;
+    }
     
- for(int i = 0;i<m;i++)
- {
-     for(int j=0;j<n;j++)
-     {
-         if(grid[i][j] == '1' && !visited[i][j])
-         {
-             solve(grid,i,j,visited);
-             count++;
-         }
-     }
- }
-    return count;
-}
-
-private void solve(char[][] grid,int i,int j,boolean[][] visited)
-{
-    if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || visited[i][j] == true || grid[i][j] == '0') return;
-  
-    visited[i][j] = true;
-
-    solve(grid,i+1,j,visited);
-    solve(grid,i-1,j,visited);
-    solve(grid,i,j+1,visited);
-    solve(grid,i,j-1,visited);
-}
+    public int sinkIsland(char[][] grid, Queue<int[]> q) {
+        int m = grid.length;
+        int n = grid[0].length;
+        while (!q.isEmpty()) {
+            int[] curr = q.remove();
+            int i = curr[0];
+            int j = curr[1];
+            if (isValid(i, j, m, n) && grid[i][j] == '1') {
+                grid[i][j] = '0';
+                q.add(new int[]{i, j + 1});
+                q.add(new int[]{i + 1, j});
+                q.add(new int[]{i - 1, j});
+                q.add(new int[]{i, j - 1});
+            }
+        }
+        return 1;
+    }
+    
+    public boolean isValid(int i, int j, int m, int n) {
+        return i >= 0 && i < m && j >= 0 && j < n;
+    }
 }
