cdfe4aa4c8d9d676620e973e221754502e7fc6f8
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-09-22 13:03:29 +0530

    Time: 17 ms (26.65%), Space: 74.3 MB (22.02%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,42 +1,43 @@
 class Solution {
-   public int numEnclaves(int[][] grid) {
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				if (i == 0 || j == 0 || i == grid.length - 1 || j == grid[i].length - 1) {
-					if (grid[i][j] == 1)
-						bfs(i, j, grid);
-				}
-			}
-		}
-		int count = 0;
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				if (grid[i][j] == 1)
-					count++;
-			}
-		}
-		return count;
-
-	}
-
-	public static void bfs(int ni, int nj, int[][] grid) {
-		int[][] dirs = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] { ni, nj });
-		boolean[][] visited = new boolean[grid.length][grid[0].length];
-		while (!q.isEmpty()) {
-			int[] r = q.poll();
-			grid[r[0]][r[1]] = 0;
-			if (visited[r[0]][r[1]])
-				continue;
-			visited[r[0]][r[1]] = true;
-			for (int d = 0; d < 4; d++) {
-				int i = r[0] + dirs[d][0];
-				int j = r[1] + dirs[d][1];
-				if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == 1 && !visited[i][j]) {
-					q.add(new int[] { i, j });
-				}
-			}
-		}
-	}
+   static int[][] dirN = {{-1,0},{0,1},{1,0},{0,-1}};
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int j=0;j<m;j++){
+            if(grid[0][j] == 1){
+                dfs(grid,0,j);
+            }
+            if(grid[n-1][j] == 1){
+                dfs(grid,n-1,j);
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(grid[i][0] == 1){
+                dfs(grid,i,0);
+            }
+            if(grid[i][m-1] == 1){
+                dfs(grid,i,m-1);
+            }
+        }
+        
+        int ans = 0;
+        for(int i=0;i<=grid.length-1;i++){
+            for(int j=0;j<=grid[0].length-1;j++){
+                if(grid[i][j] == 1){
+                  ans++;
+                }
+            }
+        }
+        return ans;
+    }
+    public void dfs(int[][] grid,int i,int j){
+        grid[i][j] = 0;
+        for(int[] dir : dirN){
+            int r = i+dir[0];
+            int c = j+dir[1];
+            if(r>=0 && c >=0 && r < grid.length && c <  grid[0].length && grid[r][c] ==1){
+                dfs(grid,r,c);
+            }
+        }
+    }
 }
