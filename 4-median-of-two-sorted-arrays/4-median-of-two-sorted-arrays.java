efc623142934527aaa6c7541f75ddc98c5ea0e91
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-09-25 10:14:12 +0530

    Time: 16 ms (14.61%), Space: 49.6 MB (64.26%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -1,44 +1,18 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
-    int l=0;
-    int r=n1;
-    
-    while(l<=r){
-        
-        int mid1=l+(r-l)/2;
-        int mid2=(n1+n2+1)/2 - mid1;
-        // we are doing +1 because we want more elements in left side. 
-        // to make buckets
-        
-        int max1= mid1==0 ? Integer.MIN_VALUE : nums1[mid1-1];
-        int max2= mid2==0 ? Integer.MIN_VALUE : nums2[mid2-1];
-        
-        int min1= mid1==n1 ? Integer.MAX_VALUE :  nums1[mid1];
-        int min2= mid2==n2 ? Integer.MAX_VALUE : nums2[mid2];
-        // when even elements where will be two median , when odd middle will be median . 
-        if(max1<=min2 && max2<=min1){
-            if((n1+n2)%2==0){
-                return ((double)Math.max(max1,max2) +  Math.min(min1,min2))/2;
-            }
-            else// odd
-                return (double)Math.max(max1,max2);
+        int n = nums1.length;
+        int m = nums2.length;
+        int[] ans = new int[n+m];
+        for(int i =0;i<n;i++){
+            ans[i]=nums1[i];
         }
-        else{
-            if(max2>min1){
-                l=mid1+1;
-            }
-            else
-                r=mid1-1;
+        for(int i=0;i<m;i++){
+            ans[n+i]=nums2[i];
         }
-        
+        Arrays.sort(ans);
+        n = n+m;
+    if(n%2==1) 
+        return ans[((n+1)/2)-1];
+    else return ((double)ans[(n/2)-1]+(double)ans[(n/2)])/2.0;
     }
-    
-    return 0.0;
-    
-    
-}
-
 }
