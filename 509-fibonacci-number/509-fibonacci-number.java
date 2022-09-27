7c0b0a738b2be1371e5e4baf9d5fb347e425f825
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-09-27 21:35:23 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (79.94%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,8 +1,17 @@
 class Solution {
+   
+        int ar[];
+    public int helper(int n)
+    {
+        if(n==0||n==1)
+           return n;
+        if(ar[n]==0)
+            ar[n]=helper(n-1)+helper(n-2);
+        return ar[n];
+    }
     public int fib(int n) {
-         if(n==0)return 0;
-        if(n <= 2)return 1;
-         else return fib(n-1) + fib(n-2);
+        ar=new int[n+2];
+        return helper(n);
     }
     
 }
