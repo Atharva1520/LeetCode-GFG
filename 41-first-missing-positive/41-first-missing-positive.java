b08983c01f9dd933d77660d223b13c3afbbf6bc9
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-02 00:22:23 +0530

    Time: 15 ms (26.74%), Space: 57.8 MB (57.53%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,15 +1,25 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-       for(int i=0;i<nums.length;i++){
-           map.put(nums[i],nums[i]);
-       }
-        int i;
-        for( i=1;i<=nums.length;i++){
-            if(!map.containsKey(i)){
-                return i;
+        int n = nums.length;
+        int corpos=0;int i=0;
+        while(i<n){
+            corpos = nums[i]-1;
+            if(corpos >= 0 && corpos < n&& nums[corpos] != nums[i]){
+                swap(i,corpos,nums);
+            }else{
+                i++;
             }
         }
-        return i;
+        for( i=0;i<n;i++){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
+    public void swap(int i,int j , int[] arr){
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
