dcf221901da0680a92372382a1e75f9af93d0a8e
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-03 15:03:16 +0530

    Time: 7 ms (85.71%), Space: 13.8 MB (30.98%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,11 +4,15 @@ public:
          if (cost.size() == 2) return min(cost[0], cost[1]);
         int n = cost.size();
         vector<int> dp(n + 1);
-        dp[0]=cost[0];
-        dp[1]=cost[1];
+      int  prev2=cost[0];
+        int prev1=cost[1];
+        int cur;
         for(int  i = 2;i<n;i++){
-            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
+            cur=cost[i]+min(prev1,prev2);
+            prev2 = prev1;
+        prev1 = cur;
         }
-        return min(dp[n-1],dp[n-2]);
+        
+        return min(prev2,prev1);
     }
 };
