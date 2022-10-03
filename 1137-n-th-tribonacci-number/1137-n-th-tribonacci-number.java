e0574435638ae9c71883ee78e4e8ef10d3b9c0c6
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-03 14:34:54 +0530

    Time: 0 ms (100.00%), Space: 39 MB (92.35%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -5,8 +5,8 @@ class Solution {
         int prev1 =1;
         int i=2;
         int cur=0;
-        if(n == 0)return 0;
-        if(n <= 2)return 1;
+        if(n == 0)return prev3;
+        if(n <= 2)return prev2;
         while(i < n){
             cur = prev3+prev2+prev1;
             i++;
