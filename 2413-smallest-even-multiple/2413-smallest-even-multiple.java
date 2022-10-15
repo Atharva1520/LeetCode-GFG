62a4fdb4e732d149cd947ec48bf29e9169236ad6
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-16 00:11:50 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (31.95%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.java
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.java
@@ -1,12 +1,5 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int num = n * 2;
-        int min = num;
-        for(int i = num ;i > 0;i--){
-            if(i % 2 == 0 && i % n == 0){
-                min = Math.min(min,i);
-            }
-        }
-        return min;
+        return n % 2 == 0 ? n : n * 2;
     }
 }
