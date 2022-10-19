8757b07a690c400cfb61842b8d728d55537f385a
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-19 21:01:48 +0530

    Time: 18 ms (10.82%), Space: 45.4 MB (42.39%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,33 +1,14 @@
 class Solution {
-    static class Pair{
-        String st;
-        int freq;
-        Pair(String st,int freq){
-            this.st =st;
-            this.freq = freq;
-        }
-    }
     public List<String> topKFrequent(String[] words, int k) {
-        HashMap<String,Integer> map = new HashMap<>();
-        for(int i=0;i<words.length;i++){
-            map.put(words[i],map.getOrDefault(words[i],0)+1);
-        }
-        PriorityQueue<Pair> pq = new PriorityQueue(new Comparator<Pair>(){
-            public int compare(Pair a,Pair b){
-                if(a.freq == b.freq){
-                    return a.st.compareTo(b.st);
-                }else
-                    return b.freq - a.freq;
-            }
-        });
-        for(Map.Entry<String,Integer> e : map.entrySet()){
-            pq.add(new Pair(e.getKey(),e.getValue()));
-        }
-        ArrayList<String> ans = new ArrayList<>();
-        for(int i=0;i<k;i++){
-            Pair r = pq.poll();
-            ans.add(r.st);
-        }
-        return ans;
+        Map<String , Integer> freq = new HashMap<>();
+    
+    for(String word : words)
+        freq.put(word,freq.getOrDefault(word , 0) + 1);
+    
+    ArrayList<String> res = new ArrayList<>(freq.keySet());
+    
+    Collections.sort(res , (a,b) -> freq.get(b) != freq.get(a) ? freq.get(b) - freq.get(a) : a.compareTo(b));
+    
+    return res.subList(0,k);
     }
 }
