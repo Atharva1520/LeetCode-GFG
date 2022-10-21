d8bc94ce35588e57f997af8edfa486217450723b
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-21 19:00:02 +0530

    Time: 1 ms (99.96%), Space: 43 MB (92.62%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> letterCasePermutation(String S) {
+        if (S == null) {
+            return new LinkedList<>();
+        }
+        
+        List<String> res = new LinkedList<>();
+        helper(S.toCharArray(), res, 0);
+        return res;
+    }
+    
+    public void helper(char[] chs, List<String> res, int pos) {
+        if (pos == chs.length) {
+            res.add(new String(chs));
+            return;
+        }
+        if (chs[pos] >= '0' && chs[pos] <= '9') {
+            helper(chs, res, pos + 1);
+            return;
+        }
+        
+        chs[pos] = Character.toLowerCase(chs[pos]);
+        helper(chs, res, pos + 1);
+        
+        chs[pos] = Character.toUpperCase(chs[pos]);
+        helper(chs, res, pos + 1);
+    }
+}
