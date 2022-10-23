4de99fd54dd6c20d20afb7067af35cc0c847ec15
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-23 11:08:22 +0530

    Time: 24 ms (19.36%), Space: 55.6 MB (14.01%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,20 +1,13 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int ans[] = new int[2];
-        int cursum=0;
-        int n = nums.length;
-        Arrays.sort(nums);
-        int sum = (n * (n+1))/2;
-       for(int i=0;i<n;i++){
-           cursum += nums[i];
-       }
-        for(int i=1;i<n;i++){
-            if(nums[i] == nums[i-1]){
-                ans[0]=nums[i];
-            }
-        }
-        cursum -= ans[0];
-        ans[1] = sum - cursum;
-        return ans;
+        Set<Integer> set = new HashSet<>();
+    int duplicate = 0, n = nums.length;
+    long sum = (n * (n+1)) / 2;
+    for(int i : nums) {
+        if(set.contains(i)) duplicate = i;
+        sum -= i;
+        set.add(i);
+    }
+    return new int[] {duplicate, (int)sum + duplicate};
     }
 }
