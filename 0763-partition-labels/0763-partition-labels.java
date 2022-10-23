406bd0e99790b4b21ba645e8a5b31c2d648a4895
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-23 16:29:33 +0530

    Time: 8 ms (63.72%), Space: 42.3 MB (76.55%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -2,15 +2,13 @@ class Solution {
     public List<Integer> partitionLabels(String s) {
          HashMap<Character,Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++){
-           char ch = s.charAt(i);
-            map.put(ch,i);
+            map.put(s.charAt(i),i);
         }
         int cur=0;
         int prev =-1;
         List<Integer> res = new ArrayList<>();
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            cur = Math.max(cur,map.get(ch));
+            cur = Math.max(cur,map.get(s.charAt(i)));
             if(cur == i){
                 res.add(cur-prev);
                 prev = cur;
