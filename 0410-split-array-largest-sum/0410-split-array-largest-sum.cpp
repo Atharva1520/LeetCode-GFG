7e3423903ec243e820df1e071aa74f0a6cbc9085
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-26 17:01:06 +0530

    Time: 1087 ms (9.17%), Space: 8.4 MB (5.43%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-     int dp[1001][51];
-    int fun(vector<int>& nums, int ind, int m){
-        if(ind == 0 && m == 0) return 0;
-        if(ind == 0 || m == 0) return INT_MAX;
-        if(dp[ind][m]!=-1) return dp[ind][m];
-        int ans = INT_MAX;
-        int cur = 0;
-        for(int k = ind; k > 0; k--){
-            cur += nums[k-1];
-            ans = min(ans, max(cur, fun(nums, k-1, m-1)));
-        }
-        return dp[ind][m] = ans;
-    }
-public:
     int splitArray(vector<int>& nums, int m) {
-        memset(dp, -1, sizeof dp);
-        return fun(nums, nums.size(), m);
+         int n = nums.size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, INT_MAX));
+        dp[0][0] = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=m; j++){
+                int cur = 0;
+                for(int k = i; k > 0; k--){
+                    cur += nums[k-1];
+                    dp[i][j] = min(dp[i][j], max(cur, dp[k-1][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
