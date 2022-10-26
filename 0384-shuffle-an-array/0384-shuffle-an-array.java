18f0b37403bb2d8f13b1d09f067fcf854adda03c
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-26 22:49:32 +0530

    Time: 121 ms (44.28%), Space: 65.9 MB (59.41%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.java
+++ 0384-shuffle-an-array/0384-shuffle-an-array.java
@@ -1,33 +1,28 @@
-import java.util.Random;
-
-public class Solution {
-    private int[] nums;
-    private Random random;
-
+class Solution {
+  int nums[];
     public Solution(int[] nums) {
         this.nums = nums;
-        random = new Random();
     }
     
-    /** Resets the array to its original configuration and return it. */
     public int[] reset() {
         return nums;
     }
     
-    /** Returns a random shuffling of the array. */
     public int[] shuffle() {
-        if(nums == null) return null;
-        int[] a = nums.clone();
-        for(int j = 1; j < a.length; j++) {
-            int i = random.nextInt(j + 1);
-            swap(a, i, j);
+        int ans[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int r = (int) (Math.random() * (i+1));
+            ans[i]=ans[r];
+            ans[r]=nums[i];
+            
         }
-        return a;
-    }
-    
-    private void swap(int[] a, int i, int j) {
-        int t = a[i];
-        a[i] = a[j];
-        a[j] = t;
+        return ans;
     }
 }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
