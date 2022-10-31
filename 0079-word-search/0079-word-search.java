ad2364f8080ce917fa7cdbfbff60e03f59790b01
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-10-31 23:09:20 +0530

    Time: 201 ms (45.44%), Space: 42 MB (62.15%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,28 +1,44 @@
 class Solution {
+     boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        int n = board.length;
-        int m = board[0].length;
-        for(int i=0;i<n;i++){
-            for(int j =0;j<m;j++){
-               if(board[i][j] == word.charAt(0) && dfs(i,j,0,board,word)){
-                   return true;
-               }
+        visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(i, j, 0, board, word)) {
+                    return true;
+                }
             }
         }
         return false;
     }
-    boolean dfs(int i,int j,int ind,char[][] board,String word){
-        if(ind == word.length())return true;
-        if(i <0 || j <0 || i == board.length || j == board[0].length || board[i][j] != word.charAt(ind)){
+    
+    private boolean dfs(int row, int col, int index, char[][] board, String word) {
+        if (checkBound(row, col, board) == -1 || 
+            visited[row][col] || 
+            word.charAt(index) != board[row][col]) {
             return false;
         }
-        char cur = board[i][j];
-        board[i][j]='-';
-        boolean found = dfs(i+1,j,ind+1,board,word)||
-                        dfs(i-1,j,ind+1,board,word)||
-                        dfs(i,j+1,ind+1,board,word)||
-                        dfs(i,j-1,ind+1,board,word);
-        board[i][j]= cur;
-        return found;
+        visited[row][col] = true;
+        if (index == word.length() - 1) {
+            return true;
+        }
+       
+        if (dfs(row + 1, col, index+1, board, word) ||
+            dfs(row, col + 1, index+1, board, word) ||
+            dfs(row - 1, col, index+1, board, word) ||
+            dfs(row, col - 1, index+1, board, word)) {
+                return true;
+            }
+        
+       
+        visited[row][col] = false;
+        return false;
+    }
+    
+    private int checkBound(int row, int col, char[][] board) {
+        if (row == -1 || row == board.length || col == -1 || col == board[0].length) {
+            return -1;
+        }
+        return 0;
     }
 }
