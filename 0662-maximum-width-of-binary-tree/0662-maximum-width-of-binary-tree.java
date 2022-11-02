1cd03fddf48fb4adc16ae32400847d64c2cd29c4
Atharva Patil <100944540+Atharva1520@users.noreply.github.com>
2022-11-02 17:10:04 +0530

    Time: 3 ms (71.00%), Space: 43.3 MB (71.49%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,23 +14,55 @@
  * }
  */
 class Solution {
-    public int widthOfBinaryTree(TreeNode root) {
-        if(root == null)return 0;
-        int ans = Integer.MIN_VALUE;
-        ArrayDeque<Pair<TreeNode,Integer>> q = new ArrayDeque<>();
-        q.add(new Pair(root,0));
-    
-        while(!q.isEmpty()){
-            int size = q.size();
-            ans = Math.max(ans,q.getLast().getValue() - q.getFirst().getValue()+1);
-            for(int i=0;i<size;i++){
-                Pair<TreeNode,Integer> p = q.poll();
-                TreeNode temp = p.getKey();
-                int ind = p.getValue();
-               if(temp.left != null) q.add(new Pair(temp.left,2*ind+1));
-               if(temp.right != null) q.add(new Pair(temp.right,2*ind+2));
-            }
+   class node{
+        
+        TreeNode data;
+        int index;
+        
+        public node(TreeNode data, int index)
+        {
+            this.data = data;
+            this.index = index;
         }
-        return ans;
+    }
+    
+    public int widthOfBinaryTree(TreeNode root) {
+        
+        if(root == null)
+            return 0;
+        
+        Queue<node> q = new LinkedList<>();
+        q.add(new node(root, 0));
+        int min = 0;
+        int max = 0;
+        int res = 1;
+        
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+    
+            for(int i = 0; i<size; i++)
+            {
+                node temp = q.poll();
+                
+                if(i == 0)
+                    min = temp.index;
+                
+                if(i == size-1)
+                    max = temp.index;
+                
+                if(temp!=null && temp.data.left!=null)
+                    q.add(new node(temp.data.left, 2*temp.index + 1));
+                
+                if(temp!=null && temp.data.right!=null)
+                    q.add(new node(temp.data.right, 2*temp.index + 2));
+                
+            }
+			
+            res = Math.max(res, max - min + 1);
+        }
+        
+        return res;
+        
     }
 }
